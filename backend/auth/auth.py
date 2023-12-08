270e80e374ad32d3a57cfdcd473fe0a61fa2dded
xbtio <yernar.qshqr01@gmail.com>
2023-12-08 20:57:20 +0600

    cookie_secure to false

--- backend/auth/auth.py
+++ backend/auth/auth.py
@@ -1,7 +1,7 @@
 from fastapi_users.authentication import CookieTransport, JWTStrategy, AuthenticationBackend
 from config import SECRET_KEY
 
-cookie_transport = CookieTransport(cookie_max_age=3600)
+cookie_transport = CookieTransport(cookie_max_age=3600, cookie_secure=False)
 
 
 SECRET = SECRET_KEY
